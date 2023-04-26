1914068e1c3434e47f32a04604888d9915335bde
Ascar <cherry400kg@gmail.com>
2023-04-26 12:05:03 +0600

    crud categories products

--- account/models.py
+++ account/models.py
@@ -1,3 +1,43 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.base_user import BaseUserManager
 
-# Create your models here.
+
+class UserManager(BaseUserManager):
+    use_in_migrations = True
+
+    def create_user(self, email, password, phone, **kwargs):
+        if not email:
+            raise ValueError('Email is required')
+        email = self.normalize_email(email)  #переводим все в нижний регистр
+        user = self.model(email=email, phone=phone, **kwargs)
+        # self.model - User
+        user.set_password(password)  # это у нас хеширование пароля
+        user.save(using=self._db)  # сохраняем юзера в базу данных
+        return user  # сохраняем юзера
+
+    # super_user имеет доступ к админке
+    def create_superuser(self, email, password, phone, **kwargs):
+        if not email:
+            raise ValueError('Email is required')
+        kwargs["is_staff"] = True
+        kwargs["is_superuser"] = True
+        kwargs["is_active"] = True
+        email = self.normalize_email(email)  # переводим все в нижний регистр
+        user = self.model(email=email, phone=phone, **kwargs)
+        # self.model - User
+        user.set_password(password)  # это у нас хеширование пароля
+        user.save(using=self._db)  # сохраняем юзера в базу данных
+        return user  # сохраняем юзера):
+
+
+class User(AbstractUser):
+    username = None  # убираем username из полей
+    email = models.EmailField(unique=True)
+    phone = models.CharField(max_length=50)
+    bio = models.TextField()
+
+    USERNAME_FIELD = 'email' # логинемся через username_field
+    REQUIRED_FIELDS = ['phone']
+
+    objects = UserManager()  # обязательно указываем менеджера
--- config/urls.py
+++ config/urls.py
@@ -15,8 +15,22 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from drf_yasg import openapi
+from drf_yasg.views import get_schema_view
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Python 23 API",
+        description="makers bootcamp",
+        default_version="v1",
+    ),
+    public=True
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('docs/', schema_view.with_ui("swagger")),
+    path('account/', include('account.urls')),
+    path('main/', include('main.urls')),
 ]
