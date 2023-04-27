170903bef6e3b217230f0b72a30f1ca7ffe05c41
Ascar <cherry400kg@gmail.com>
2023-04-27 12:14:57 +0600

    comments raiting favourite paggination

--- config/settings.py
+++ config/settings.py
@@ -145,7 +145,9 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
 
         'rest_framework_simplejwt.authentication.JWTAuthentication',
-    )
+    ),
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 6,
 
 }
 
@@ -160,3 +162,10 @@ SWAGGER_SETTINGS = {
 }
 
 AUTH_USER_MODEL = 'account.User'
+
+from datetime import timedelta
+
+SIMPLE_JWT = {
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=60),
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
+}
